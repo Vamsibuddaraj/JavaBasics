2d6df47dd2f5447dbcf28ae9357d2d91fe7cdb0d
vamsi <vamsi@LAPTOP-29TBJGVM.T-mobile.com>
2021-10-21 12:45:26 +0530

    commit

---
+++ Dxc/src/Methods/FindEvenOdd.java
@@ -0,0 +1,23 @@
+package Methods;
+import java.util.*;
+
+public class FindEvenOdd {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int number = input.nextInt();
+		OddEven(number);
+	}
+	static void OddEven(int number) {
+		if(number%2 == 0) {
+			
+			System.out.println(number+" is an even number"); 
+			
+		}else {
+			
+			System.out.println(number+" is an odd number");
+			
+		}
+	}
+}
