ac86caf5bd851865c1c124a26f4c10b6b9196b3d
vamsi <vamsi@LAPTOP-29TBJGVM.T-mobile.com>
2021-10-21 12:33:59 +0530

    commit

---
+++ Dxc/src/ControlStatements/IfElseStatement.java
@@ -0,0 +1,14 @@
+package ControlStatements;
+
+public class IfElseStatement {
+	
+	public static void main(String[] args) {
+		int age=20;
+		if(age>18) {
+			System.out.println("age is greater then 18");
+		}else {
+			System.out.println("age is less then 18");
+		}
+		
+	}
+}
