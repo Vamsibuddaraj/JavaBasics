b2bc7ffc3615322636521150e1446e4c81aab61f
vamsi <vamsi@LAPTOP-29TBJGVM.T-mobile.com>
2021-10-21 12:28:15 +0530

    commit

---
+++ Dxc/src/Constructors/Student.java
@@ -0,0 +1,17 @@
+package Constructors;
+
+public class Student {
+	int age;
+	String name;
+	
+	public Student(int Age,String Name){
+		this.age=Age;
+		this.name=Name;
+	}
+	public Student() {
+		
+	}
+	public void Data() {
+		System.out.println("Student name is "+name+" and age is "+age);
+	}
+}
