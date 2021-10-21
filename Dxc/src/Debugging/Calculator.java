584fee8190a44f2ddab335a53ea42b07b7b9db3c
vamsi <vamsi@LAPTOP-29TBJGVM.T-mobile.com>
2021-10-21 12:36:33 +0530

    commit

---
+++ Dxc/src/Debugging/Calculator.java
@@ -0,0 +1,13 @@
+package Debugging;
+
+public class Calculator {
+	
+	int add(int num1,int num2) {
+		return num1-num2;
+	}
+	void addding() {
+		for(int i=0;i<=10;i++) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ Dxc/src/Debugging/Debug.java
@@ -0,0 +1,11 @@
+package Debugging;
+
+public class Debug {
+	
+	public static void main(String[] args) {
+		Calculator c1=new Calculator();
+		int sum=c1.add(11, 22);
+		System.out.println("the sum is-"+sum);
+		c1.addding();
+	}
+}
