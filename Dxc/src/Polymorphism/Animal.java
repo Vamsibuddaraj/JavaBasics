75d63bde9c312b025ba555e8eb18b11a6fc0c6eb
vamsi <vamsi@LAPTOP-29TBJGVM.T-mobile.com>
2021-10-21 14:27:10 +0530

    commit

---
+++ Dxc/src/Polymorphism/Animal.java
@@ -0,0 +1,13 @@
+package Polymorphism;
+
+class Animal {
+	Animal(){
+		System.out.println("animal is created");
+		} 
+}
+class Dog extends Animal{  
+	Dog(){  
+		//super();  
+		System.out.println("dog is created");  
+	}  
+}  
