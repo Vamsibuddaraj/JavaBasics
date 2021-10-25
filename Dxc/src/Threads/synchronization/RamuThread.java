cbc0113686cbfd47d91f06696e964f3284f2bd30
vamsi <vamsi@LAPTOP-29TBJGVM.T-mobile.com>
2021-10-25 16:19:15 +0530

    commit

---
+++ Dxc/src/Threads/synchronization/RamuThread.java
@@ -0,0 +1,17 @@
+package Threads.synchronization;
+
+public class RamuThread extends Thread{
+	Table mTable;
+	
+	public RamuThread(Table table) {
+		mTable = table;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+		mTable.printTable(5);
+	}
+
+}
