23e05216ab074015cc9ca7bbf7417137b5714f8a
vamsi <vamsi@LAPTOP-29TBJGVM.T-mobile.com>
2021-10-25 15:53:56 +0530

    commit

---
+++ Dxc/src/Threads/Runtime1.java
@@ -0,0 +1,8 @@
+package Threads;
+
+public class Runtime1 {
+	
+	public static void main(String args[])throws Exception{  
+		Runtime.getRuntime().exec("mspaint");//will open a new notepad  
+	}  
+}
